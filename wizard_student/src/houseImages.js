1bc83fc7c998e481f49168802624eae6222f471e
Nicholas Freeman <nick_freeman65@yahoo.com>
2022-04-06 16:00:39 -0500

    finish images for house

---
+++ wizard_student/src/houseImages.js
@@ -0,0 +1,22 @@
+export const houseImages = [
+    {
+        id: 1,
+        house: "Ravenclaw",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsbGJpBDp02PgfDfsPnqBxiijME0H72a78jQ&usqp=CAU"
+    },
+    {
+        id: 2,
+        house: "Gryffindor",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfXbBZ81qua_xvVV0zwtjuRonIRBgT_FHkyukzuZv6JDEjQ5ZVpbE91k2q9opf2z0Fa3o&usqp=CAU"
+    },
+    {
+        id:3,
+        house: "Hufflepuff",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWZLIzn5Hn4LGftdPhG34x1Sipy96fdFkR1A&usqp=CAU"
+    },
+    {
+        id:4,
+        house: "Slytherin",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCRYuFqRxCOmsGOCLsaQBm_Lt671FQGWGekA&usqp=CAU'
+    }
+]
