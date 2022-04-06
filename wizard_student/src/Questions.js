cff3f9a2e22f03278458214c2f4a78797fa8cfb4
Nicholas Freeman <nick_freeman65@yahoo.com>
2022-04-06 15:52:39 -0500

    make questions for sorting quiz

---
+++ wizard_student/src/Questions.js
@@ -0,0 +1,112 @@
+export const questions = [
+    {
+        id: 1,
+        question: "Which of these traits best describe you?",
+        answers: [
+            {
+                answer: "Loyal",
+                score: "H"
+            },
+            {
+                answer: "Cunning",
+                score: "S"
+            },
+            {
+                answer: "Brave",
+                score: "G"
+            },
+            {
+                answer: "Clever",
+                score: "R"
+            }
+        ]
+    },
+    {
+        id: 2,
+        question: "What kind of animal would you be?",
+        answers: [
+            {
+                answer: "Owl",
+                score: "R"
+            },
+            {
+                answer: "Hippogriff",
+                score: "G"
+            },
+            {
+                answer: "Cat",
+                score: "H"
+            },
+            {
+                answer: "Lizard",
+                score: "S"
+            }
+        ]
+    },
+    {
+        id: 3,
+        question: "What is your worst trait?",
+        answers: [
+            {
+                answer: "Recklessness",
+                score: "G"
+            },
+            {
+                answer: "Lack of knowledge",
+                score: "R"
+            },
+            {
+                answer: "Too nice",
+                score: "R"
+            },
+            {
+                answer: "I don't know",
+                score: "S"
+            }
+        ]
+    },
+    {
+        id: 4,
+        question: "Which would be a great way to spend the day?",
+        answers: [
+            {
+                answer: "Hanging out with friends",
+                score: "H"
+            },
+            {
+                answer: "Exploring new places",
+                score: "G"
+            },
+            {
+                answer: "Reading a good book",
+                score: "R"
+            },
+            {
+                answer: "Crafting or building",
+                score: "S"
+            }
+        ]
+    },
+    {
+        id: 5,
+        question: "What super power would you have?",
+        answers: [
+            {
+                answer: "Invisibility",
+                score: "H"
+            },
+            {
+                answer: "Foresight",
+                score: "R"
+            },
+            {
+                answer: "Super strength",
+                score: "G"
+            },
+            {
+                answer: "Mind control",
+                score: "S"
+            }
+        ]
+    }
+]
