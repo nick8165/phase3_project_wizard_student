da72f51695490f031af286e7ffe04d6bb0707380
Nicholas Freeman <nick_freeman65@yahoo.com>
2022-04-06 15:46:03 -0500

    create switch for state

--- wizard_student/src/Sorting.js
+++ wizard_student/src/Sorting.js
@@ -5,7 +5,32 @@ import { houseImages } from "./houseImages";
 
 function Sorting({studentProfile, handleRerender}) {
     
-    
+    const [answer, setAnswer] = useState({one: "", two: "", three: "", four: "", five: ""})
+
+    function handleOptionChange(e) {
+        let form = e.target.closest("Form")
+        
+        switch(form.id) {
+            case("1"):
+                setAnswer({...answer, one: e.target.value})
+                break;
+            case("2"):
+                setAnswer({...answer, two: e.target.value})
+                break;
+            case("3"):
+                setAnswer({...answer, three: e.target.value})
+                break;
+            case("4"):
+                setAnswer({...answer, four: e.target.value})
+                break;
+            case("5"):
+                setAnswer({...answer, five: e.target.value})
+                break;
+            default:
+                console.log(e.target.value)
+                break;
+        }
+      }
 
     return (
         <Container style={{ width: '25rem',  }} className="mb-5">
